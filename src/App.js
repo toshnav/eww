fe455705fb8fa444ac837a69d5d0ce78bcb5f7c6
Toshnav Vinayak Khatke <56795862+toshnav@users.noreply.github.com>
2022-04-30 01:53:11 +0530

    hooks knowldge

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import ClassCounter from './components/ClassCounter';
+import HookCounter from './components/HookCounter';
+import HookCounterFour from './components/HookCounterFour';
+import HookCounterThree from './components/HookCounterThree';
+import HookCounterTwo from './components/HookCounterTwo';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <HookCounterFour/>
+      
     </div>
   );
 }
